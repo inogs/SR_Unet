ee0561dc8d9c1ea1962d00be36c1c2582b5c5fb3
anna del savio <adelsavi@lrdn1862.leonardo.local>
2026-03-24 17:54:15 +0100

    risolto problema shape della mask

--- make_pt_ds_optim.py
+++ make_pt_ds_optim.py
@@ -62,88 +62,62 @@ def get_xy_single_var(cms_path:str, ogs_path:str, cms2ogs_map:Dict[str, str], cm
 
 
 
-# normalization with broadcasting
-# def normalize(ds:np.array, means: np.array, stds:np.array, mask:np.array):
-#     ds = ds[:, None, :, :, :] # Samples, Channel = None = 1, Depth 27, Height 300, Width 494
-#     mask = np.repeat(mask, ds.shape[0], axis=0)
-#    # print(ds.shape)
-#    # print(mask.shape)
-#     ds = np.ma.masked_array(ds, mask)
 
-#     # creiamo shape dinamica
-#     shape = [1, -1] + [1]*(ds.ndim-2)
 
+
+# normalization with for loop
+def normalize(ds:np.array, means:np.array, stds:np.array, mask:np.array):
+    """Compute normalization of the dataset, given its mean
+       and standard deviation.
+       MASK: array booleano della stessa forma spaziale di un singolo campione, che indica quali elementi devono essere ignorati nella normalizzazione.
+       Le posizioni mascherate vengono infine riempite con 1e7 per indicare valori “inutilizzabili”.
+    """
+    print('input mask shape', mask.shape) # dovrebbe essere (1 = C, 27, 300, 494)
+    mask = np.repeat(mask, ds.shape[0], axis=0) # Qui la maschera originale, che copre solo le dimensioni spaziali di un singolo campione, viene ripetuta lungo l’asse dei campioni, in modo che corrisponda alla forma completa di ds.
+    # print(ds.shape[0])
+    ds = np.ma.masked_array(ds, mask) # crea un array mascherato, dove tutti gli elementi dove mask=True vengono ignorati nelle operazioni matematiche.
+    normalized_ds = np.zeros_like(ds, dtype=np.float32)
+    print('mask shape2:', mask.shape)
+    print('ds shape:', ds.shape)
+    # print(ds.ndim)
+    # NOTA: questi due cicli for probabilmente potrebbero essere sostituiti con qualocsa di più efficiente -> BROADCASTING ?
+    for i in range(ds.shape[0]):  # Iterate over each data sample
+        for v in range(ds.shape[1]):  # Iterate over each channel
+           normalized_ds[i, v, :, :, :] = np.ma.masked_invalid((ds[i, v, :, :, :] - means[v]) / stds[v]).filled(1e7)
+
+    print('output shape:', normalized_ds.shape)
+    return normalized_ds.data
+
+
+# def normalize(ds: np.array, means: np.array, stds: np.array, mask: np.array):
+#     """
+#     Normalizza un dataset multi-canale con broadcasting.
+    
+#     ds: np.array, shape = (samples, channels, depth, H, W) 
+#     means: np.array, shape = (channels,)
+#     stds: np.array, shape = (channels,)
+#     mask: np.array, shape = (depth, H, W) per singolo campione
+#     """
+#     # ripeti la mask lungo samples
+#     print('ds input shape:', ds.shape)
+#     ds = ds[:, :, :, :]  # aggiungi asse channel = 1
+#     print('ds shape:', ds.shape)
+#     mask = np.repeat(mask, ds.shape[0], axis=0)  
+#     ds = np.ma.masked_array(ds, mask)                      # shape = (samples, 1, depth?, H, W)
+#     print('mask shape:', mask.shape)
+#     print('ds shape after masking:', ds.shape)
+    
+#     # broadcasting dei canali
+#     shape = [1, -1] + [1]*(ds.ndim-2)  # [1, channels, 1,1,1,...] per diff dimensioni spaziali
 #     ds = (ds - means.reshape(shape)) / stds.reshape(shape)
-#     # print('mean shape:', means.shape)
-#     # print('sd shape:', stds.shape)
+    
 #     ds = np.ma.masked_invalid(ds).filled(1e7)
-#     # print(ds.shape)
+#     print('output shape', ds.shape)
 #     return ds.astype(np.float32)
 
-def normalize(ds: np.array, means: np.array, stds: np.array, mask: np.array):
-    """
-    Normalizza un dataset multi-canale con broadcasting.
-    
-    ds: np.array, shape = (samples, channels, depth, H, W) 
-    means: np.array, shape = (channels,)
-    stds: np.array, shape = (channels,)
-    mask: np.array, shape = (depth, H, W) per singolo campione
-    """
-    # ripeti la mask lungo samples
-    print('ds input shape:', ds.shape)
-    ds = ds[:, None, :, :, :]  # aggiungi asse channel = 1
-    print('ds shape:', ds.shape)
-    mask = np.repeat(mask, ds.shape[0], axis=0)  
-    ds = np.ma.masked_array(ds, mask)                      # shape = (samples, 1, depth?, H, W)
-    print('mask shape:', mask.shape)
-    print('ds shape after masking:', ds.shape)
-    
-    # broadcasting dei canali
-    shape = [1, -1] + [1]*(ds.ndim-2)  # [1, channels, 1,1,1,...] per diff dimensioni spaziali
-    ds = (ds - means.reshape(shape)) / stds.reshape(shape)
-    
-    ds = np.ma.masked_invalid(ds).filled(1e7)
-    print('output shape', ds.shape)
-    return ds.astype(np.float32)
 
 
-
-# # normalization with for loop
-# def normalize(ds:np.array, means:np.array, stds:np.array, mask:np.array):
-#     """Compute normalization of the dataset, given its mean
-#        and standard deviation.
-#        MASK: array booleano della stessa forma spaziale di un singolo campione, che indica quali elementi devono essere ignorati nella normalizzazione.
-#        Le posizioni mascherate vengono infine riempite con 1e7 per indicare valori “inutilizzabili”.
-#     """
-#     ds = ds[:, None, :, :, :] 
-#     mask = np.repeat(mask, ds.shape[0], axis=0) # Qui la maschera originale, che copre solo le dimensioni spaziali di un singolo campione, viene ripetuta lungo l’asse dei campioni, in modo che corrisponda alla forma completa di ds.
-#     ds = np.ma.masked_array(ds, mask) # crea un array mascherato, dove tutti gli elementi dove mask=True vengono ignorati nelle operazioni matematiche.
-#     normalized_ds = np.zeros_like(ds, dtype=np.float32)
-#     print('mask shape:', mask.shape)
-#     print('ds shape:', ds.shape)
-
-
-#     print(ds.ndim)
-#     # NOTA: questi due cicli for probabilmente potrebbero essere sostituiti con qualocsa di più efficiente
-#     for i in range(ds.shape[0]):  # Iterate over each data sample
-#         for v in range(ds.shape[1]):  # Iterate over each channel
-#             if ds.ndim == 4:  # Check if the image is 2D or 3D
-#                 normalized_ds[i, v, :, :] = np.ma.masked_invalid((ds[i, v, :, :] - means[v]) / stds[v]).filled(1e7) # .masked_invalid maschera eventuali valori NaN o inf risultanti dalla divisione, sostituendoli con con 1e7
-#             else:
-#                 normalized_ds[i, v, :, :, :] = np.ma.masked_invalid((ds[i, v, :, :, :] - means[v]) / stds[v]).filled(1e7)
-
-#     # # MODIFICA QUI PER DEBUG
-#     # print("\n[DEBUG normalize] dtype:", ds.dtype)
-#     # print("[DEBUG normalize] min/max:", ds.min(), ds.max())
-#     # print("[DEBUG normalize] std min:", np.min(stds))
-#     print('norms ds:', normalized_ds.shape)
-#     return normalized_ds.data
-
-
-
-
-
-def make_var_dataset(cms_path:str, ogs_path:str, save_path:str, stat_path:str, train_path:str, var: str, cms2ogs_map:Dict[str, str], split:str):
+def make_var_dataset(cms_path:str, ogs_path:str, save_path:str, stat_path:str, train_path:str, var_list: List[str], cms2ogs_map:Dict[str, str], split:str): 
     '''
         Saves the Pytorch dataset corresponding to a given variable in the output folder.
 
@@ -161,70 +135,86 @@ def make_var_dataset(cms_path:str, ogs_path:str, save_path:str, stat_path:str, t
 
     cms = []
     ogs = []
+    varx = []
 
     x_means = []
     x_stds = []
     y_means = []
     y_stds = []
 
-    # leggo 
-    varx_list, vary_list = get_xy_single_var(cms_path, ogs_path, cms2ogs_map, var, split)
-    cms.append(varx_list)
-    ogs.append(vary_list)
+    for var in var_list:
+        # leggo 
+        varx_list, vary_list = get_xy_single_var(cms_path, ogs_path, cms2ogs_map, var, split)
+        cms.append(varx_list)
+        ogs.append(vary_list)
 
 
-    # prendo medie e sd per la normalizzazione 
-    with open(f'{stat_path}/cms/stat_cms_{var}.txt', 'r') as file:
-        line_elements = []
-        for line in file:
-            if line.strip():
-                line_elements.append(np.float32(line))
-    x_means.append(line_elements[0])
-    x_stds.append(line_elements[1])
+        # prendo medie e sd per la normalizzazione 
+        with open(f'{stat_path}/cms/stat_cms_{var}.txt', 'r') as file:
+            line_elements = []
+            for line in file:
+                if line.strip():
+                    line_elements.append(np.float32(line))
+        x_means.append(line_elements[0])
+        x_stds.append(line_elements[1])
 
-    with open(f'{stat_path}/ogs/stat_ogs_{var}.txt', 'r') as file:
-        line_elements = []
-        for line in file:
-            if line.strip():
-                line_elements.append(np.float32(line))
-    y_means.append(line_elements[0])
-    y_stds.append(line_elements[1])
+        with open(f'{stat_path}/ogs/stat_ogs_{var}.txt', 'r') as file:
+            line_elements = []
+            for line in file:
+                if line.strip():
+                    line_elements.append(np.float32(line))
+        y_means.append(line_elements[0])
+        y_stds.append(line_elements[1])
 
     x_means = np.array(x_means)
     x_stds = np.array(x_stds)
     y_means = np.array(y_means)
     y_stds = np.array(y_stds)
 
-    x = cms[0]
-    y = ogs[0]
+    x = np.array(list(zip(*cms)))
+    y = np.array(list(zip(*ogs)))
+
+
 
     
     # Calcola mask solo sul training set
     if split == 'train':
-        mask = x[0] > 100000
+        print('x shape:', x.shape)
+        mask = x[0] > 100000 # np.any(x > 100000, axis=0)
     else:
-        # Legge solo il CMS train path per calcolare la maschera
-        varx_train, _ = get_xy_single_var(train_path, None, cms2ogs_map, var, 'train')
-        mask = varx_train[0] > 100000
-        # cms_train = []
-        # varx_list, _ = get_xy_single_var(train_path, None, cms2ogs_map, var, split)
-        # cms_train.append(varx_list)
-        # x = np.array(cms_train)
-        # mask = x[0] > 100000
+        # Legge solo il CMS train path per calcolare la maschera 
+        # GIUSTO SE LE MASCHERE SONO UGUALI per tutti i timestamp !
+        # qui aggiungo un for loop altrimenti l'output mashera mi dava shape sbagliata, cioè senza la shape channel, dato che per tutte le variabili CREDO ci siano le stesse maschere
+        # invece di iterare per le variabili si potrebbe fare un reshape della maschera e basta 
+        for v in var_list:
+            varx_train, _ = get_xy_single_var(train_path, None, cms2ogs_map, var, 'train') 
+            varx.append(varx_train)
+        varx  = np.array(list(zip(*varx)))
+        # print('varx', varx.shape) # dovrebbe essere (717, 1, etc) -> risolto
+        mask = varx[0] > 100000 # np.any(x > 100000, axis=0)
+
+    # print('print mask shape', mask.shape)
 
     x = normalize(x, x_means, x_stds, mask)
     y= normalize(y, y_means, y_stds, mask)
     torch_ds = TensorDataset(torch.Tensor(x), torch.Tensor(y))
 
  
+    if len(var_list) == len(cms2ogs_map):
+        name = "all_"
+    else:
+        name = ""
+        for var in var_list:
+            name = name + f"{var}_"
+
     os.makedirs(save_path, exist_ok=True)
-    ds_save_path = os.path.join(save_path,f"{var}_{split}_dataset.pt")
+    ds_save_path = os.path.join(save_path,f"{name}_{split}_dataset.pt")
     print(f"Saving the {split} pytorch datasets in {save_path}")
     torch.save(torch_ds, ds_save_path)
 
 
 
-
+# CREA FUNZIONE CHE CONTROLLA IL CONF
 
 if __name__== "__main__":
     """Script to construct the training and the test dataset
@@ -240,6 +230,7 @@ if __name__== "__main__":
     save_path = None
     split = None
     conf_path = None
+    var_list = []
 
     while i < len(sys.argv):
 
@@ -253,40 +244,58 @@ if __name__== "__main__":
             conf_path = sys.argv[i+1]; i += 2
         
         elif sys.argv[i] == "-v":
-            if var: raise ValueError("Repeated input for variable")
-            var = sys.argv[i+1]; i += 2
+            if var_list != []: raise ValueError("Repeated input for variable")
+            if sys.argv[i+1] == "all":
+                var_list = "all"
+                i+= 2
+            else:
+                while i < len(sys.argv)-1:
+                    if not sys.argv[i+1].startswith('-'):
+                        var_list.append(sys.argv[i+1]) ; i+= 1
+                    else:
+                        break
+                i+= 1
 
         elif sys.argv[i] == "-split":
             if split: raise ValueError('Repeated input for -split')
             split = sys.argv[i+1]; i += 2
 
        
-        
+    # controllo sullo split   
     if split not in ["train", "test", "validation"]:
             raise ValueError("split must be train, test or validation")
 
-    if not var:
-        print("[WARNING] No variables provided with -v. No variable dataset will be created.")
-
+    # lettura config
     with open(conf_path, 'r') as f:
         conf = json.load(f)
     conf = obj(conf)
 
- 
+    # lettura dizionario cms <-> ogs 
     with open(conf.map_path, 'r') as f:
         cms2ogs_map = json.load(f)
 
-    print(f"[make_dataset for variables {var}] Starting execution")
+    # opzione 'considera tutte le variabili'
+    if var_list == "all":
+        var_list = list(cms2ogs_map.keys())  
 
+# CHIAMA FUNZIOME CHE CONTROLLA IL CONF 
+
+    # controllo su var list
+    if var_list == []:
+        print("[WARNING] No variables provided with -v. No variable dataset will be created.")
+    else:
+        print(f"[make_dataset for variables {var_list}] Starting execution")
+
+    # salviamo il path del train
     cms_train_path = conf.cms_train_path
 
-    if var is not None:
+    # chiamiamo funzione che crea i file .pt 
+    if var_list != []:
         if split == 'train':
-            make_var_dataset(conf.cms_train_path,conf.ogs_train_path, save_path, conf.stat_path, cms_train_path, var, cms2ogs_map, split)
+            make_var_dataset(conf.cms_train_path,conf.ogs_train_path, save_path, conf.stat_path, cms_train_path, var_list, cms2ogs_map, split)
         elif split == 'test':
-            make_var_dataset(conf.cms_test_path,conf.ogs_test_path, save_path, conf.stat_path, cms_train_path, var, cms2ogs_map, split)
+            make_var_dataset(conf.cms_test_path,conf.ogs_test_path, save_path, conf.stat_path, cms_train_path, var_list, cms2ogs_map, split)
         else:
-            make_var_dataset(conf.cms_val_path,conf.ogs_val_path, save_path, conf.stat_path, cms_train_path, var, cms2ogs_map, split)
-
+            make_var_dataset(conf.cms_val_path,conf.ogs_val_path, save_path, conf.stat_path, cms_train_path, var_list, cms2ogs_map, split)
 
     

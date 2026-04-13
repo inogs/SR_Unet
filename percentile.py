aea4ee925becb7621611c4d9bc40315ad4d9da4c
anna del savio <adelsavi@login02.leonardo.local>
2026-04-13 17:26:18 +0200

    modifiche

--- percentile.py
+++ percentile.py
@@ -1,3 +1,6 @@
+# script che calcola il quinto percentile unendo i ds train test e val della variabile che gli passi per i mesi che mettendo insieme i dati per il primo layer (idem per secondo e terzo) 
+# -> esempio 1 output: quinto percentile del primo layer di tutti i dati di giugno etc
+
 import numpy as np
 import matplotlib.pyplot as plt
 import netCDF4 as nc
@@ -9,7 +12,8 @@ from matplotlib.colors import ListedColormap
 import os
 from scipy.ndimage import uniform_filter
 import natsort
-import subprocess
+from collections import Counter
+
 
 
 def name_components_ave(filename):
@@ -51,14 +55,9 @@ def find_month(num: int):
         return "December"
 
 
-def analyze_multi_dataset(data_dirs, var_name, output_txt):
-    print('sono qui')
-
-    months_target = ["June", "July", "August"]
+def analyze_multi_dataset(data_dirs, var_name, output_txt, months_target, extra_tag):
 
     data = {m: {0: [], 1: [], 2: []} for m in months_target}
-
-    print('sono qui1')
 
     for data_dir in data_dirs:
         var_dir = os.path.join(data_dir, var_name)
@@ -67,17 +66,21 @@ def analyze_multi_dataset(data_dirs, var_name, output_txt):
 
         if not os.path.exists(var_dir):
             print("MISSING:", var_dir)
-            continue
+            break
 
         files = natsort.natsorted([
             f for f in os.listdir(var_dir) if f.endswith(".nc")
         ])
 
-        print('sono qui2', len(files))
+
+        counter = Counter()
+
+
 
         for fname in files:
-            var, time = name_components_ave(fname)
+            _ , time = name_components_ave(fname)
             month = find_month(time)
+            counter[month] += 1
 
             print('file:', fname, month)
 
@@ -93,6 +96,9 @@ def analyze_multi_dataset(data_dirs, var_name, output_txt):
                 data[month][layer].append(var_data[layer].flatten())
 
             ds.close()
+        
+        print(counter)
+
 
     # =========================
     # CALCOLO PERCENTILI
@@ -109,6 +115,7 @@ def analyze_multi_dataset(data_dirs, var_name, output_txt):
                 continue
 
             all_values = np.concatenate(data[month][layer])
+           #  print(len(all_values)) # 10670400: 72 (file .nc di quel mese) * lat * lon * 1(depth perchè prendo uno alla volta i primi tre layer)
             p5 = np.nanpercentile(all_values, 5)
 
             lines.append(f"Layer {layer} - p5: {p5}")
@@ -128,12 +135,20 @@ def analyze_multi_dataset(data_dirs, var_name, output_txt):
     if ext == "":
         ext = ".txt"
 
-    final_output = os.path.join(output_dir, f"{name}_{var_name}{ext}")
+
+    final_name = f"{name}_{var_name}"
+
+    if extra_tag is not None:
+        final_name += f"_{extra_tag}"
+
+    final_output = os.path.join(output_dir, final_name + ext)
 
     with open(final_output, "w") as f:
         f.write("\n".join(lines))
 
     print(f"Saved to {final_output}")
+
+
 
 
 
@@ -143,4 +158,25 @@ if __name__ == "__main__":
     var_name = sys.argv[2]
     output_txt = sys.argv[3]
 
-    analyze_multi_dataset(data_dirs, var_name, output_txt)
+    # opzionale
+    extra_tag = None
+    if "-altro" in sys.argv:
+        idx = sys.argv.index("-altro")
+        if idx + 1 < len(sys.argv):
+            extra_tag = sys.argv[idx + 1]
+
+
+    months_target = []
+    if "-months" in sys.argv:
+        idx = sys.argv.index("-months")
+        if idx + 1 < len(sys.argv):
+            months_target = [
+                m.strip() for m in sys.argv[idx + 1].split(",")
+            ]
+
+    # DEFAULT se non passi nulla
+    if months_target == []:
+        months_target = ["January", "February", "March", "April", "May", "June",
+                     "July", "August", "September", "October", "November", "December"]
+
+    analyze_multi_dataset(data_dirs, var_name, output_txt, months_target, extra_tag)

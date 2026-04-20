6fbb8383ed40d36c7d7b73059b94eda0201fced6
Giacomo Zuccarino <gzuccarino@ogs.it>
2026-04-20 18:22:46 +0200

    initial structure

--- preproc/split_train_test_WIP.py
+++ preproc/split_train_test_WIP.py
@@ -9,45 +9,19 @@ import random
 
 seed = 42
 
-# Lo script vuole mantenere corrispondenza temporale tra tutte le variabili, quindi: Seleziona file solo da una variabile di riferimento (var_list[0]), Usa quei file selezionati anche per tutte le altre variabili e per i river
-
-def confirm_split(files_to_move):
-    print("\n==============================")
-    print("ATTENZIONE: stai per splittare il dataset.")
-    print(f"Numero di timestamp selezionati: {len(files_to_move)}")
-    print("I file verranno SPOSTATI")
-    print("==============================")
-
-    answer = input("Sei sicuro di voler continuare? (yes/no): ")
-
-    if answer.lower() not in ["yes", "y"]:
-        print("Operazione annullata.")
-        sys.exit(0)
-
-    print("Conferma ricevuta. Procedo con lo split.\n")
 
 # THIS MUST BECOME GET FILE TO MOVE AND THEN YOU MOVE ALL OF THEM
-def get_random_files(source_dir:str, percentage:float, reference_var:str) -> List[str]:
-
-# AGGIUNGO SEED PER LA RIPRODUCIBILITA'
+# get two lists as return value: the first one contains the files to move, the second one contains the remaining files
+def get_random_files(source_dir:str, percentage:float) -> (List[str], List[str]):
+    
     random.seed(seed)
 
-    '''
-        Sample a list of files to move from the source directory to the folder of the test set, ensuring that each season is equally represented.
-
-        Args:
-            source_dir (str): source directory
-            percentage (float): percentage of data to be sampled
-            refernce_var (str): reference var (folder) from which we sample files randomly
-        Returns:
-            List sampled file names (List[str])
-    '''
-    # We look at the reference_var folder to select random time instants for the test set
-    source_dir = os.path.join(source_dir, reference_var)
     file_list = os.listdir(source_dir)
-    # Divide files in the 4 seasons
+    file_list = [f for f in file_list if os.path.isfile(os.path.join(source_dir, f))]
+
     seasons = {"winter" : [], "spring" : [], "summer" : [], "autumn" : []}
     for file_name in file_list:
+        # print("file name:", file_name)
         t = int(file_name[-6:-3])
         if 0 <= t < 18:
             seasons["winter"].append(file_name)
@@ -67,46 +41,34 @@ def get_random_files(source_dir:str, percentage:float, reference_var:str) -> Lis
         random_files = random.sample(l, num_files)
         # Add files to the list
         files_to_move += random_files
-    return files_to_move
+    
+    print(f"Selected {len(files_to_move)} files to move, which corresponds to {len(files_to_move)/len(file_list)*100:.2f}% of the total files.")
+    print(f"Selected files: {files_to_move}")
 
-def move_files(files_to_move:List[str], source_dir:str, dest_dir:str, var_name:str, is_cms:bool, bar:Any) -> None:
-    '''
-        Moves all files related to the names in the files_to_move list to the destination directory.
+    # take a second list of the remaining files
+    other_files = [f for f in file_list if f not in files_to_move]
+    # print number of remaining files
+    print(f"Remaining files: {len(other_files)}")
 
-        Args:
-            files_to_move (List[str]): list of file names related to the files to be moved.
-            source_dir (str): source directory.
-            dest_dir (str): destination directory.
-            var_name (str): name of the biogeochemical variable in the source directory.
-            is_cms (bool): whether the variable name follows the CMS nomeclature ore the OGS one.
-        Returns:
-            List sampled file names (List[str])
-    '''
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-    file_list = os.listdir(source_dir)
-    # Iterate over the files in the first source directory
+    return files_to_move, other_files
+
+def move_files(files_to_move:List[str], source_dir:str, dest_dir:str) -> None:
+
+    os.makedirs(dest_dir, exist_ok=True)
+
     for file in files_to_move:
-        # Extract the corresponding file name
-        name_end = file[4:] if is_cms else file[4:].replace("-", "_")
-        if var_name == "river":
-            name_end = name_end.replace("nc", "txt")
-        reference_name =  var_name + "_" + name_end
-        if (reference_name in file_list):
-            source_path = os.path.join(source_dir, reference_name)
-            # Move file with name reference_name to the destination directory
-            shutil.move(source_path, dest_dir)
-            bar()
+        file_src_path = os.path.join(source_dir, file)
+        file_dst_path = os.path.join(dest_dir, file)
 
-if __name__ == '__main__':
-    '''
-        Splits the dataset into train and test set.
+        if not os.path.isfile(src):
+            raise FileNotFoundError(f"Source file does not exist: {file_src_path}")
+        # end if
+        
+        shutil.move(file_src_path, file_dst_path)
+    # end for
+# end def (move_files)
 
-        Parameters:
-        -dp data path
-        -ts indicates the percentage of data included in the test set. Example: for a test size of 20% write "-ts 0.2".
-        -val if we have to split train and validation set 
-    '''
+def read_input_parameters():
     i = 1
     test_size = None
     data_path = None
@@ -126,64 +88,43 @@ if __name__ == '__main__':
         else:
             i+=1
     if data_path is None: raise TypeError("Missing value for data path")
+    # exit(0)
     if test_size is None: raise TypeError("You need to specify the size of the test set")
+    # exit(0)
 
-    map_path = os.path.join(data_path, "cms2ogs.json")
+    print(f"Data path: {data_path}")
+    print(f"Test size: {test_size}")
+    print(f"Split train and validation set: {val}")
 
-    with open(map_path, 'r') as f:
-        cms2ogs_map = json.load(f)
-
-    var_list = list(cms2ogs_map.keys())
+    return data_path, test_size, val
+# end def
     
+
+if __name__ == '__main__':
+    '''
+        Splits the dataset into train and test set.
+
+        Parameters:
+        -dp data path
+        -ts indicates the percentage of data included in the test set. Example: for a test size of 20% write "-ts 0.2".
+        -val if we have to split train and validation set 
+    '''
+   
+    data_path, test_size, val = read_input_parameters()
+    train_path = os.path.join(data_path, "var_train")
+    test_path = os.path.join(data_path, "var_test")
+    # check if directories exist, if not create them
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+    if not os.path.exists(test_path):
+        os.makedirs(test_path)
     
-
-    
-    if val:
-        cms_path_3D = os.path.join(data_path, "iCMS_nc") # train copernicus 
-        ogs_path_3D = os.path.join(data_path, "NARF_nc") # train ogs cadeau
-        cms_test_path_3D = os.path.join(data_path,"nc_iCMS_val")
-        ogs_test_path_3D = os.path.join(data_path, "nc_OGS_val")
-
-    # mancano i dataset interi da cui fare split train e test (abbiamo solo AdriatticNC che osno i copernicus ma che non sono interpolati, e mancano i Cadeau)
-    # else: 
-    #     cms_path_3D = os.path.join(data_path, "iCMS_nc")
-    #     ogs_path_3D = os.path.join(data_path, "NARF_nc")
-    #     cms_test_path_3D = os.path.join(data_path,"nc_iCMS_test")
-    #     ogs_test_path_3D = os.path.join(data_path, "nc_OGS_test")
-
-
     percentage_to_move = float(test_size)
 
-    # Get files to move for for one of the biogeochemical variables
-    #files_to_move = get_random_files(cms_path_3D, percentage_to_move, var_list[0])
-    #MODIFICA TOLGO CASO SURFACE
-    reference_path = cms_path_3D
-    files_to_move = get_random_files(reference_path, percentage_to_move, var_list[0])
+    test_files_list, train_files_list = get_random_files(data_path, percentage_to_move)
 
-    # ti chiede conferma se vuoi procedere con lo splittamento dei datasets
-    # NB. DA TOGLIERE SE SI LANCIA CON SLURM! 
-    confirm_split(files_to_move)
 
     print(f"[split_test_train with test size {test_size}] Starting execution")
-    with alive_bar(0, title=f"Moving files...") as bar:
-        for var in var_list:
-
-            # Splitting 3D data
-            source_dir_cms = os.path.join(cms_path_3D, var)
-            source_dir_ogs = os.path.join(ogs_path_3D, cms2ogs_map[var])
-            dest_dir_cms = os.path.join(cms_test_path_3D, var)
-            dest_dir_ogs = os.path.join(ogs_test_path_3D, cms2ogs_map[var])
-
-            move_files(files_to_move, source_dir_cms, dest_dir_cms, var, True, bar)
-            move_files(files_to_move, source_dir_ogs, dest_dir_ogs, cms2ogs_map[var], False, bar)
-            
-
-        # Splitting rivers data
-        if val:
-            source_dir_riv = os.path.join(data_path, "rivers", "rivers_train")
-            dest_dir_riv = os.path.join(data_path, "rivers", "vector_val")
-        else:
-            source_dir_riv = os.path.join(data_path, "rivers", "vector")
-            dest_dir_riv = os.path.join(data_path, "rivers", "vector_test")
-        move_files(files_to_move, source_dir_riv, dest_dir_riv, "river", False, bar)
+    move_files(test_files_list, source_dir = data_path, dest_dir = test_path)
+    move_files(train_files_list, source_dir = data_path, dest_dir = train_path)
     print(f"[split_test_train with test size {test_size}] Ending execution")

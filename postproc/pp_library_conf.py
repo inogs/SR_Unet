2d7fbb6de7c73a71b51984f29c61d6e93227fff5
Giacomo Zuccarino <gzuccarino@ogs.it>
2026-06-24 14:24:20 +0200

    create conf library, update split months

---
+++ postproc/pp_library_conf.py
@@ -0,0 +1,15 @@
+import json
+from types import SimpleNamespace
+
+
+def read_json_conf(path):
+    with open(path) as f:
+        return json.load(f, object_hook=lambda d: SimpleNamespace(**d))
+
+
+def read_split_months_conf(path):
+    return read_json_conf(path)
+
+
+def read_compute_monthly_stats_conf(path):
+    return read_json_conf(path)
--- postproc/split_months.py
+++ postproc/split_months.py
@@ -1,14 +1,4 @@
-import argparse
 import os
-import re
-import time
-from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Manager
-from pathlib import Path
-from types import SimpleNamespace
-import netCDF4 as nc
-import numpy as np
-import numpy.ma as ma
 
 from pp_library import (
     extract_year_and_period_id,
@@ -19,16 +9,21 @@ from pp_library import (
     read_file_list
 )
 
-path_output = os.path.join(os.path.dirname(__file__), "out.months")
-path_input = "/leonardo/home/userexternal/gzuccari/git/OGS/SR_Unet/postproc/input.folder/Chla.test.txt"
-print(path_output)
-print(path_input)
+from pp_library_conf import ( read_split_months_conf)
 
+# setup block
+conf_split_months_path = os.path.join(os.path.dirname(__file__), "conf_split_months.json")
+conf_split_months = read_split_months_conf(conf_split_months_path)
+print(f"Configuration for split months: {conf_split_months}")
+path_input = conf_split_months.input_path
+path_output = conf_split_months.output_path
+folder_name = conf_split_months.output_folder_name
 
+# initial structures
 list_input_files = read_file_list(path_input)
 dist_output_month_files = make_month_file_dict()
 
-
+# get months
 for file_name in list_input_files:
     year, period_id = extract_year_and_period_id(file_name)
     season_name = determine_season(period_id)
@@ -41,7 +36,8 @@ print("Number of files in each month:")
 for month, files in dist_output_month_files.items():
     print(f"{month}: {len(files)}")
 
-path_output_folder = os.path.join(path_output, "Chla.test.months")
+# print outputs to file
+path_output_folder = os.path.join(path_output, folder_name)
 os.makedirs(path_output_folder, exist_ok=True)
 for month, files in dist_output_month_files.items():
     month_id = MONTH_IDS[month]
